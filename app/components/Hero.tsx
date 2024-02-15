c9cbdd8c422d27b58f78a26c3c175f4680aef33a
Tom <twallace1015@gmail.com>
2024-02-15 15:28:20 -0500

    hero component started

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import mdesignlogo from "../../images/mdesignlogo.png";
+import Image from "next/image";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className='flex flex-col md:flex-row items-center justify-center gap-8 p-4 w-11/12 md:w-3/4 lg:w-7/10 mx-auto'>
+      <div className='flex-1 flex justify-center'>
+        <Image
+          src={mdesignlogo}
+          alt={"logo"}
+          width={250} // Increased size for larger screens
+          height={250} // Maintain aspect ratio
+          className='rounded-lg'
+        />
+      </div>
+      <div className='flex-1 flex flex-col items-center md:items-start'>
+        <h1 className='text-3xl md:text-4xl font-bold text-center md:text-left'>
+          Hi, I'm Melissa
+        </h1>
+        <p className='mt-4 text-center md:text-left md:max-w-md lg:max-w-lg'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='navbar bg-base-100'>
+      <div className='flex-1'>
+        <a className='btn btn-ghost text-xl'>MDesigns</a>
+      </div>
+      <div className='flex-none'>
+        <ul className='menu menu-horizontal px-1'>
+          <li>
+            <a>Portfolio</a>
+          </li>
+          <li>
+            <a>Contact Me</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
